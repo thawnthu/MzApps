2d73102724c49a71f9ac790d0f8ca44c9afd03c8
thawnthu <thawn.thu95@gmail.com>
2026-08-25 20:52:28 +0530

    Update MainActivity.kt

--- app/src/main/java/com/mzchat/MainActivity.kt
+++ app/src/main/java/com/mzchat/MainActivity.kt
@@ -1,20 +1,19 @@
-package com.mzchat;
-import android.app.Activity;
-import android.os.Bundle;
-import android.widget.TextView;
-import android.view.Gravity;
+package com.mzchat
+import android.app.Activity
+import android.os.Bundle
+import android.widget.TextView
+import android.view.Gravity
 
-public class MainActivity extends Activity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         
-        TextView tv = new TextView(this);
-        tv.setText("MzApps - A Tlang Fel Ta!\n\nWelcome Boss!");
-        tv.setTextSize(24);
-        tv.setGravity(Gravity.CENTER);
-        tv.setPadding(50, 200, 50, 50);
+        val tv = TextView(this)
+        tv.text = "MzApps - A Tlang Fel Ta!\n\nWelcome Boss!"
+        tv.textSize = 24f
+        tv.gravity = Gravity.CENTER
+        tv.setPadding(50, 200, 50, 50)
         
-        setContentView(tv);
+        setContentView(tv)
     }
 }
